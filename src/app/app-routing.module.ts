e866d08831c4ab838173a91367e52d9540f2918d
maison <s>
2020-05-12 08:25:17 +0200

    firt comit sitvoyage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultPageComponent } from './default-page/default-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+  path: 'defaultPage',
+  component: DefaultPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultPageComponent } from './default-page/default-page.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { CardPostComponent } from './card-post/card-post.component';
+import { MenuEnteteComponent } from './menu-entete/menu-entete.component';
+import { LiensService } from './services/liens.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultPageComponent,
+    CardListComponent,
+    CardPostComponent,
+    MenuEnteteComponent,
+    
+
   ],
   imports: [
     BrowserModule,
